2e95504e7a2cb7cae9dacac3d51c54d4595d5b87
crysska <chriskabuya@ymail.com>
2017-03-12 20:38:03 -0400

    [NXTS-001] - Initial commit

---
+++ pmukendi/helper/sub-menu.php
@@ -0,0 +1,12 @@
+<?php
+$nom = "LA GLOBULE";
+// $nom contient alors la chaîne de caractères LA GLOBULE.
+$current = "class='current'";
+// $mon_chiffre contient la valeur numérique 12.
+?>
+<ul>
+  <li><a href="events.php">Events</a></li>
+  <li><a href="weddings.php">Weddings</a></li>
+  <li><a href="fashions.php">Fashions</a></li>
+  <li><a href="randoms.php">Randoms</a></li>
+</ul>
